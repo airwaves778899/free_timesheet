3726d444eb493b87e48108cd3d784eea28aa0c7a
bryan <bryanlin1121@gmail.com>
2016-06-20 10:45:44 +0800

    new

---
+++ src/main/java/free/timesheet/FreeTimesheetApplication.java
@@ -0,0 +1,12 @@
+package free.timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTimesheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeTimesheetApplication.class, args);
+	}
+}
